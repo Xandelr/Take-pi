26c99b84ad70c4dd217e498523f204e65e8ac299
Xandelr <xanderanimekun@gmail.com>
2025-05-10 18:59:39 -0400

    Update Temperatura.py

--- Temperatura.py
+++ Temperatura.py
@@ -22,14 +22,14 @@ print("Conectado a WiFi, dirección IP:", wlan.ifconfig())
 i2c = I2C(0, sda=Pin(0), scl=Pin(1))
 sensor = bmp280.BMP280(i2c)
 
-# Webhook de Google Sheets
-url = 'https://script.google.com/macros/s/AKfycbxzfHsNl-DLwNCCgPF-mzDmTI3i9OTsrpTNCGTYPJMgGVXKCVbj4Imt_b7zFvvvIOfVog/exec'
+# Webhook URL de Google Apps Script
+url = 'https://script.google.com/macros/s/AKfycbxLcFQBHFkTahQ0mQHLCYCHaUL6ZlD7K1kpctc9Z1KZio_9YH8VgLrysDNUaVczzcO8Qg/exec'
 
 while True:
-    gc.collect()  # libera memoria
+    gc.collect()
     temp = sensor.get_temperature()
 
-    # Obtener la hora del dispositivo (hora local del microcontrolador)
+    # Formatear fecha
     fecha = time.localtime()
     fecha_str = "{:02d}/{:02d}/{:04d} {:02d}:{:02d}:{:02d}".format(
         fecha[2], fecha[1], fecha[0], fecha[3], fecha[4], fecha[5]
@@ -38,14 +38,15 @@ while True:
     print("Temperatura:", temp)
     print("Fecha:", fecha_str)
 
+    # Crear el cuerpo tipo formulario
+    data = "temperatura={}&fecha={}".format(temp, fecha_str)
+
     try:
-        response = urequests.post(url, json={
-            "temperatura": temp,
-            "fecha": fecha_str
-        })
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response = urequests.post(url, data=data, headers=headers)
         print("Respuesta:", response.text)
         response.close()
     except Exception as e:
         print("Error al enviar los datos:", e)
 
-    sleep(60)  # espera 1 minuto
+    sleep(60)
