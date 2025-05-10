535d1439eb52a3a3ca957fbf1bbea570f32a27b4
Xandelr <xanderanimekun@gmail.com>
2025-05-09 23:16:42 -0400

    Update Temperatura.py

--- Temperatura.py
+++ Temperatura.py
@@ -1,27 +1,30 @@
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials, firestore
 from machine import I2C, Pin
-from time import sleep
 import bmp280
+from time import sleep
 
-# Inicializa Firebase
-cred = credentials.Certificate("C:\Users\cecer\Downloads\takepi-314-firebase-adminsdk-fbsvc-782a65dd10.json")  
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://takepi-314.firebaseio.com/'
-})
+# Inicializa Firebase Admin
+cred = credentials.Certificate('/path/to/tu/archivo/credenciales.json')
+firebase_admin.initialize_app(cred)
 
-# Inicializar I2C en GP0 (SDA) y GP1 (SCL)
+# Configura I2C
 i2c = I2C(0, sda=Pin(0), scl=Pin(1))
-
-# Crear objeto del sensor
 bmp = bmp280.BMP280(i2c)
 
+# Configura Firestore
+db = firestore.client()
+
 while True:
+    # Obtén la temperatura
     temperatura = bmp.get_temperature()
     print("Temperatura: {:.2f} °C".format(temperatura))
 
-    # Subir la temperatura a Firebase
-    ref = db.reference('pisos/piso3/temperatura') 
-    ref.set(temperatura)
-
-    sleep(60)  # Enviar cada 60 segundos (ajusta el intervalo según lo necesites)
+    # Subir la temperatura a Firestore (piso2, mesa01)
+    doc_ref = db.collection(u'mesas').document(u'piso2').collection(u'mesa01')
+    doc_ref.update({
+        u'temperatura': temperatura
+    })
+    
+    # Espera 10 segundos antes de obtener una nueva temperatura
+    sleep(10)
